442996ffd46b9dd34cbc70ad4231552c8898c263
derick maradiaga <>
2021-04-20 06:21:52 +0000

    tried to get how many got above 80

--- midterm/mid1.cpp
+++ midterm/mid1.cpp
@@ -1,3 +1,5 @@
+//write of the program
+
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -29,3 +31,46 @@ int main()
     }
     ofs.close();
 }
+
+
+
+
+//read of program
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    string studentname;
+    int total_stud;
+    int score1, score2;
+    double sum, avg;
+    ifstream ifs;
+
+    ifs.open("student.txt");
+    if (ifs.fail())
+    {
+        cerr << "File Open Error \n";
+        exit(0);
+    }
+    ifs >> total_stud;
+    sum = 0;
+    for (int i = 0; i < total_stud; i++)
+    {
+        ifs >> studentname >> score1 >> score2 ;
+        cout << " student name : " << studentname << "\t";
+        cout << " Score 1 : " << score1 << " Score 2 : " << score2 <<"\t";
+        
+        sum = score1 + score2;
+        cout << "sum: " << sum << endl;
+
+        avg = sum / 2;
+        cout << "Avg : " << avg << endl;
+    
+    }
+    for (int i = 0; i < total_stud; i++)
+         ifs >> score1 >> score2 ;
+            if(avg > 80)
+                cout <<  << endl;
