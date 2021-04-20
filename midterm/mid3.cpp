375400f1947d441206ae04290b21382c36f1153e
derick maradiaga <>
2021-04-20 21:32:44 +0000

    10 min commit

--- midterm/mid3.cpp
+++ midterm/mid3.cpp
@@ -4,7 +4,8 @@
 #include <cstdlib>
 using namespace std;
 int getRdnum (void);
-int getMinandMax (int , int, int);
+int getMax (int , int, int);
+int getMin (int , int, int);
 int differance(int , int);
 
 
@@ -23,9 +24,11 @@ int main()
 
      cout << num1 <<" " << num2 << " " << num3 << endl;
 
-    Max = getMinandMax (num1 , num2, num3)
+    Max = getMax (num1 , num2, num3);
 
-    Min = 
+    Min = getMin (num1 , num2, num3);
+
+    cout << Max << " " << Min << endl;
         
 
 
@@ -41,11 +44,43 @@ int getRdnum ()
 } 
 
 
-int getMinandMax (int , int, int)
+int getMax (int number1, int number2, int number3)
 {
+    //max code
+     if (number1 > number2 && number1 > number3)
+     {
+        return number1;
+     }
+     
+     else if (number2 > number1 && number2 > number3)
+     {
+        return number2;
+     }
+    
+     else if (number3 > number1 && number3 > number2)
+     {
+         return number3;
+     }
+ return false;
+}
 
+int getMin (int number1, int number2, int number3)
+{
+     // min code 
+    if (number1 < number2 && number1 < number3)
+     {
+        return number1;
+     }
+     
+     else if (number2 < number1 && number2 < number3)
+     {
+        return number2;
+     }
+    
+     else if (number3 < number1 && number3 < number2)
+     {
+         return number3;
+     }
 
-
-
-
+return false;
 }
