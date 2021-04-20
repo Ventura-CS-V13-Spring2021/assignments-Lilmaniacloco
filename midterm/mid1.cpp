8d99696ee7690ba4c5ee0d5155d5f6db6dd03231
derick maradiaga <>
2021-04-20 05:54:13 +0000

    add write file

---
+++ midterm/mid1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    string studentname;
+    int total_stud;
+    int score1, score2;
+    ofstream ofs;
+
+    ofs.open("student.tx");
+    if (ofs.fail())
+    {
+        cerr << "File Op[en Error\n";
+        exit(0);
+    }
+    cout << "Enter the number of students : ";
+    cin >> total_stud;
+    ofs << total_stud << endl;
+    for (int i = 0; i < total_stud; i++)
+    {
+        cout << "Enter the student name : ";
+        cin >> studentname;
+        cout << "Enter the student top two scores: ";
+        cin >> score1 >> score2;
+       
+        ofs << studentname<< " \n" << score1 << " " << score2 << endl;
+    }
+    ofs.close();
+}
