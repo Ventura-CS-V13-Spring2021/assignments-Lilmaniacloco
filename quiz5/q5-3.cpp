da2655e4b5dbfb42f0d445f46e643df20a1069eb
derick maradiaga <>
2021-04-26 20:57:36 +0000

    starting q3

---
+++ quiz5/q5-3.cpp
@@ -0,0 +1,42 @@
+#include    <iostream>
+using namespace std;
+void makebinary(int [], int);
+void printbinary(int [], int);
+void count0cluster(int [], int);
+int main()
+{
+    const int SIZE = 20;
+    int     binary[SIZE];
+
+    srand(time(0));
+    makebinary(binary, SIZE);
+    printbinary(binary, SIZE);
+    count0cluster(binary, SIZE);
+}
+void makebinary(int num[], int size)
+{
+    for(int i=0;i<size; i++)
+        num[i] = rand() % 2;
+}
+void printbinary(int num[], int size)
+{
+    for(int i=0;i<size; i++)
+        cout << num[i] << " ";
+    cout << endl;
+}
+void count0cluster(int bin[], int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (bin[i]== 0)
+        {
+            count++;
+            continue;
+        }
+
+    }
+    cout << " Count of 0 clusters are: " << count << endl;
+
+
+}
