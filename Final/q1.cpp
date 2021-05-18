d8b7ba37a31bf6576e8bfd2a085b4add5852a9d4
derick maradiaga <>
2021-05-18 20:29:05 +0000

    add more

--- Final/q1.cpp
+++ Final/q1.cpp
@@ -1,22 +1,55 @@
 #include <iostream>
+#include <ctime>
+#include  <cstdlib> 
+ #include <algorithm>
 using namespace std;
 
 int getNumDiv(int []);
+void makearray(int []);
+void printarray(int []);
+
 
 int main() {
-  int arr[6] = {3,5,16,25,26,30};
-  int num = getNumDiv(arr);
-  cout << "The number with the most factors is: " << num << endl;
+    int arr[]={};
+    srand(time(0));
+    makearray(arr);
+    sort(arr, arr + 6);
+  
+    printarray(arr);
+ // int arr[6] = {3,5,16,25,26,30};
+  int n= getNumDiv(arr);
+  cout << "The number with the most factors is: " << n << endl;
 
   return 0;
 }
 
+void makearray(int num[])
+{
+    for(int i=0;i<6; i++)
+        num[i] = rand() % 100;
+}
+
+
+void printarray(int num[])
+{
+    for(int i=0;i<6; i++)
+        cout << num[i] << "\t";
+    cout << endl;
+}
+
 int getNumDiv(int numbers[]) {
-  //count # of factors
-  int count = 0, prevCount = 0, maxNum = 0;
+  
+  int count = 0, prevCount = 0, max = 0;
 
   for (int i=0; i<6; i++) {
     count = 0;
     for (int j=2; j<numbers[i]; j++) {
       if ((numbers[i] % j) == 0) {
         count += 1;
+      }
+    }
+    if (count >= prevCount)
+      max = numbers[i];
+  }
+  return max;
+}
